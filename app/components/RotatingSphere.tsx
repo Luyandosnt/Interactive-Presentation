3eae4c7ab236ed3110b585650ad284b297fdd0e7
Luyando Shilukukwa <48803829+Luyandosnt@users.noreply.github.com>
2025-10-26 20:14:56 +0200

    Update RotatingSphere.tsx

--- app/components/RotatingSphere.tsx
+++ app/components/RotatingSphere.tsx
@@ -5,13 +5,13 @@ import type React from "react"
 import { useEffect, useState, useRef, useMemo } from "react"
 
 const images = [
-  "/assets/1.jpg",
-  "/assets/2.jpg",
-  "/assets/3.jpg",
-  "/assets/4.jpg",
-  "/assets/5.jpg",
-  "/assets/6.jpg",
-  "/assets/7.jpg",
+  "/assets/1.jpeg",
+  "/assets/2.jpeg",
+  "/assets/3.jpeg",
+  "/assets/4.jpeg",
+  "/assets/5.jpeg",
+  "/assets/6.jpeg",
+  "/assets/7.jpeg",
 ]
 
 const Bubble: React.FC<{ size: number; left: string; delay: number; duration: number }> = ({
